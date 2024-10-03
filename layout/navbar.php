0154798bac22948f0485639ce3cfcd9380f1aa60
BSIT-Sanchez <sanchezlando333@gmail.com>
2024-10-04 00:55:35 +0800

    added

---
+++ layout/navbar.php
@@ -0,0 +1,26 @@
+<div class="bg-blue-600">
+        <div class="container mx-auto">
+            <nav class='flex justify-between items-center p-2 relative'>
+                <div>
+                <img src='images/logo.png' alt="logo" />
+                </div>
+
+                <form class=' hidden md:flex border justify-between items-center rounded-sm p-[2px] px-2 bg-white w-[30%]'>
+                    <input type="text" placeholder='search...' class='outline-none p-[2px] rounded-sm bg-white w-[95%]'/>
+                    <i class="fas fa-search cursor-pointer"></i>
+                        
+                </form>
+
+                <div class=' justify-center items-center gap-4 hidden md:flex'>
+                    <a href="/aboutUs" class="font-[500]">Services</a>
+                    <a href="/aboutUs" class="font-[500]">About</a>
+                    <a href="/contactUs" class="font-[500]">Contact us</a>
+                    <a href="/civil-registrar/pages/login.php" class="font-[500] border px-2 py-1 rounded-md">Sign In</a>
+                    
+            
+                </div>
+            </nav>
+        </div>
+        
+     
+      </div>
